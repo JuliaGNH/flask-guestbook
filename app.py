ce7c54f9eb5e395a101aaae199a36d3ffd681896
Your Name <you@example.com>
2019-10-24 12:02:20 -0400

    Fix style issues

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 import configparser
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
@@ -14,23 +13,29 @@ if 'db' in config:
     db_pass = config['db']['MYSQL_DATABASE_PASSWORD']
     db_name = config['db']['MYSQL_DATABASE_DB']
     db_host = config['db']['MYSQL_DATABASE_HOST']
-    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://%s:%s@%s/%s" % (db_user, db_pass, db_host, db_name)
+    app.config['SQLALCHEMY_DATABASE_URI'] = (
+            "mysql://%s:%s@%s/%s" % (db_user, db_pass, db_host, db_name))
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite://"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+
 class Signatures(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String(1000))
 
+
 db.create_all()
+
 
 @app.route('/signatures', methods=['GET'])
 def sign():
     result = Signatures.query.all()
     return "\n".join(map(lambda x: x.message, result))
+
 
 @app.route('/signatures', methods=['POST'])
 def write():
@@ -40,6 +45,7 @@ def write():
     db.session.commit()
 
     return ""
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
